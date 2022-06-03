a107e26805226dfabe021c71f733661cef660f84
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-06-03 14:53:17 +0530

    add

---
+++ hexanovate/src/components/Home.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import '../styles/Home.css'
+
+export const Home = () => {
+
+const [movies,setMovies] =useState([]);
+const [filter,setFilter] = useState([])
+
+useEffect(() => {
+    fetchMovies();
+},[])
+
+const fetchMovies = () => {
+    fetch ("https://hexanovate-1oc3v5uf6-thephenom1708.vercel.app/api/movies")
+    .then((res) =>  res.json())
+    .then((res) => {
+      console.log(res)
+      setMovies(res)
+      setFilter(res)  
+    })
+};
+console.log(movies)
+    return(
+        <>
+            <h1>Movies</h1>
+            
+            <div className='main'>
+                {filter.map((el) => {
+                    return ( 
+                    <>
+                        <div className='container'>
+                            <div>
+                                <img src = {el.poster} alt ="images"></img>
+                            </div>
+                            <span>{el.title}</span>
+                            <span>{el.genre}</span>
+                            <span>{el.imdbRating}</span>
+                            <span>{el.released}</span>
+
+                        </div>
+                    </>
+                    )
+                })}
+            </div>
+            
+        </>
+    )
+}
