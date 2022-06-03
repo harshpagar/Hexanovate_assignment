7b585d29d08f588a54292d15fc33e5321e36a648
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-06-03 15:05:17 +0530

    add

--- hexanovate/src/components/Home.jsx
+++ hexanovate/src/components/Home.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import '../styles/Home.css'
-
 export const Home = () => {
 
 const [movies,setMovies] =useState([]);
@@ -24,20 +23,23 @@ console.log(movies)
         <>
             <h1>Movies</h1>
             
-            <div className='main'>
+            <div className='main' >
+
                 {filter.map((el) => {
                     return ( 
                     <>
                         <div className='container'>
-                            <div>
-                                <img src = {el.poster} alt ="images"></img>
-                            </div>
-                            <span>{el.title}</span>
-                            <span>{el.genre}</span>
-                            <span>{el.imdbRating}</span>
-                            <span>{el.released}</span>
+                           <div className='img'>
+                               <img src = {el.poster} alt ="images"></img>
+                           </div>
+                           <span>{el.title}</span>
+                           <span>{el.genre}</span>
+                           <span>{el.imdbRating}</span>
+                           <span>{el.released}</span>
 
-                        </div>
+                       </div>
+                       
+                       
                     </>
                     )
                 })}
